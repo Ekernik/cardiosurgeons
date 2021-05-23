1813e30188e3fddc86167b4ccd554c32a253aadd
Ekernik <ekernik@gmail.com>
2021-05-23 14:39:22 +0300

    Create Landing page

---
+++ src/pages/Landing.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Landing(){
+    return(
+        <div>
+            {/* HEADER */}
+            {/* SECTION */}
+                {/* GREETING SECTION */}
+                {/* ABOUT US */}
+                {/* OUR TEAM */}
+                {/* SERVICES */}
+                {/* CONTACTS */}
+            {/* FOOTER */}
+        </div>
+    )
+}
+
+export default Landing;
