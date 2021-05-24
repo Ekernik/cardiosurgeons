3f422438b41610d234a0db791fdf0b4c6eadc8bc
Ekernik <ekernik@gmail.com>
2021-05-24 08:30:14 +0300

    Add basic styles for Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,38 @@
 import React from 'react';
+import '../css/header.scss';
 
 import logo from '../assets/images/logo_1.png';
 
 function Header() {
+    let handleClick = () => {
+        const headerBurger = document.querySelector('.header__menu-burger');
+        const headerMenu = document.querySelector('.header__burger-nav');
+        const body = document.querySelector('body');
+        body.classList.toggle('fixed-page');
+        headerMenu.classList.toggle('menu--opened');
+        headerBurger.classList.toggle('menu--opened');
+    };
+
     return(
         <header className='header'>
             <div className='container'>
-                <nav className='header__nav'>
+                <nav className='header__navbar'>
                     <img className='logo' src={logo} alt=''/>
                     <ul className='header__menu'>
+                        <li><a href="#landing-greeting" className='menu__item'>главная</a></li>
+                        <li><a href="#landing-about-us" className='menu__item'>о нас</a></li>
+                        <li><a href="#landing-our-team" className='menu__item'>команда</a></li>
+                        <li><a href="#landing-services" className='menu__item'>услуги</a></li>
+                        <li><a href="#landing-contacts" className='menu__item'>контакты</a></li>
+                    </ul>
+                </nav>
+                <div onClick={handleClick} className='header__burger'>
+                    <div className="header__menu-burger">
+                        <span></span>
+                    </div>
+                </div>
+                <nav className='header__burger-nav'>
+                    <ul className='burger__menu'>
                         <li><a href="#landing-greeting" className='menu__item'>главная</a></li>
                         <li><a href="#landing-about-us" className='menu__item'>о нас</a></li>
                         <li><a href="#landing-our-team" className='menu__item'>команда</a></li>
