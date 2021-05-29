2c06de5f4d9083ebaa6a7c695b7b0c33d0e2b375
Ekernik <ekernik@gmail.com>
2021-05-29 17:21:07 +0300

    Create Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../css/footer.scss';
+
+function Footer() {
+    return(
+        <footer className='footer'>
+            <div className='container'>
+                <span className='footer__copyright'>
+                    © copyright 2021
+                </span>
+            </div>
+        </footer>
+    ) 
+}
+
+export default Footer;
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function Landing(){
     return(
@@ -12,7 +13,7 @@ function Landing(){
                 {/* OUR TEAM */}
                 {/* SERVICES */}
                 {/* CONTACTS */}
-            {/* FOOTER */}
+            <Footer />
         </div>
     )
 }
