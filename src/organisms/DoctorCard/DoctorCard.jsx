200733c2656d125b1256b5457612bb6158752c3b
Ekernik <ekernik@gmail.com>
2021-08-08 15:14:11 +0300

    Add fake links and hover effects to DoctorCard

--- src/organisms/DoctorCard/DoctorCard.jsx
+++ src/organisms/DoctorCard/DoctorCard.jsx
@@ -4,9 +4,10 @@ import './index.scss';
 export default function DoctorCard(props) {
     return(
         <div className='doc-card__doctor'>
-            <img className='doc-card__img' src={props.imgSrc} alt={props.imgAlt} />
-            <h2 className='doc-card__title'>{props.title}</h2>
+            <a href='#TODO'><img className='doc-card__img' src={props.imgSrc} alt={props.imgAlt} /></a>
+            <a href='#TODO' className='doc-card__title'><h2>{props.title}</h2></a>
             <h3 className='doc-card__subtitle'>{props.subtitle}</h3>
+            <a href="#TODO" className="doc-card__about-link">читать подробнее</a>
         </div>
     )
 }
