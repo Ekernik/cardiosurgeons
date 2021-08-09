ddd38648002a2a7c7af393f059a59729dfd2b130
Ekernik <ekernik@gmail.com>
2021-08-09 23:56:01 +0300

    Fix links

--- src/pages/Treatment/Treatment.jsx
+++ src/pages/Treatment/Treatment.jsx
@@ -12,28 +12,28 @@ export default function Treatment() {
       <div className="container container__services">
         <h1 className="services__header">Лечение</h1>
         <div className="services__list">
-          <a className="services__link" href="/hot/coronarography">
+          <a className="services__link" href="/treatment/coronarographia">
             Коронарография
           </a>
-          <a className="services__link" href="/hot/operation_02">
+          <a className="services__link" href="/treatment/stentirovanie-koronarnih-arteriy">
             Стентирование коронарных артерий
           </a>
-          <a className="services__link" href="/hot/operation_03">
+          <a className="services__link" href="/treatment/aorto-koronarnoe-shuntirovanie">
             Аорто-коронарное шунтирование
           </a>
-          <a className="services__link" href="/hot/operation_04">
+          <a className="services__link" href="/treatment/plastika-mitralnogo-klapana">
             Пластика митрального клапана
           </a>
-          <a className="services__link" href="/hot/miniinvisiv">
+          <a className="services__link" href="/treatment/miniinvisivnoe-protezirovanie-klapanov">
             Миниинвизивное протезирование клапанов
           </a>
-          <a className="services__link" href="/hot/protezirovanie">
+          <a className="services__link" href="/treatment/protezirovanie-aorti">
             Протезирование аорты
           </a>
-          <a className="services__link" href="/hot/operation_07">
+          <a className="services__link" href="/treatment/karotidnaya-endarterektomiya">
             Каротидная эндартерэктомия
           </a>
-          <a className="services__link" href="/hot/operation_08">
+          <a className="services__link" href="/treatment/radiochastotnaya-ablyaciya">
             Радиочастотная абляция
           </a>
         </div>
