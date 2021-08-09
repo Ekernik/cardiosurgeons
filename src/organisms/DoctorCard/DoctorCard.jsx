8752c1c25c5316de991b9a299d638a75bb6c7504
Ekernik <ekernik@gmail.com>
2021-08-10 01:09:10 +0300

    Hide About doctors button on DoctorCard component

--- src/organisms/DoctorCard/DoctorCard.jsx
+++ src/organisms/DoctorCard/DoctorCard.jsx
@@ -7,7 +7,7 @@ export default function DoctorCard(props) {
             <a href='#TODO'><img className='doc-card__img' src={props.imgSrc} alt={props.imgAlt} /></a>
             <a href='#TODO' className='doc-card__title'><h2>{props.title}</h2></a>
             <h3 className='doc-card__subtitle'>{props.subtitle}</h3>
-            <a href="#TODO" className="doc-card__about-link">читать подробнее</a>
+            {/* <a href="#TODO" className="doc-card__about-link">читать подробнее</a> */}
         </div>
     )
 }
