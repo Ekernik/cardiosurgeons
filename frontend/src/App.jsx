534f7e82df7de58f50ff906c0f708a5c05d4b874
Ekernik <ekernik@gmail.com>
2021-10-10 14:07:09 +0300

    Add doctor Kim personal page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,21 +2,21 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.scss";
 
 import Landing from "./pages/Landing.jsx";
-import Coronarography from "./pages/treatment/Coronarography";
+import Coronarography from "./pages/treatments/Coronarography";
 import News from "./pages/news/";
 import Licenses from "./pages/Licenses";
 import PageNotFound from "./pages/404/PageNotFound";
 import OurTeam from "./pages/OurTeam/OurTeam";
-import Stentirovanie from "./pages/treatment/Stentirovanie";
-import Shuntirovanie from "./pages/treatment/Shuntirovanie";
-import PlastikaMK from "./pages/treatment/PlastikaMK";
-import Karodit from "./pages/treatment/Karotid";
-import RadioAblacia from "./pages/treatment/RadioAblacia";
+import Stentirovanie from "./pages/treatments/Stentirovanie";
+import Shuntirovanie from "./pages/treatments/Shuntirovanie";
+import PlastikaMK from "./pages/treatments/PlastikaMK";
+import Karodit from "./pages/treatments/Karotid";
+import RadioAblacia from "./pages/treatments/RadioAblacia";
 import Treatment from "./pages/Treatment/Treatment";
 import OurContacts from "./pages/OurContacts/OurContacts";
-import Diagnostics from "./pages/Diagnostics/Diagnostics";
+import Diagnostics from "./pages/Diagnostic/Diagnostics";
 import AboutUs from "./pages/AboutUs/AboutUs";
-import miniinvisiv from "./pages/treatment/Miniinvisiv";
+import miniinvisiv from "./pages/treatments/Miniinvisiv";
 
 import news from "./assets/databases/news";
 import Kotova from "./pages/doctors/Kotova";
@@ -28,6 +28,7 @@ import Shmatov from "./pages/doctors/Shmatov";
 import Ateroskleros from "./pages/diagnostics/Ateroskleros";
 import Stolyarov from "./pages/doctors/Stolyarov";
 import Kamenskih from "./pages/doctors/Kamenskih";
+import Kim from "./pages/doctors/Kim";
 
 function App() {
   return (
@@ -82,6 +83,7 @@ function App() {
           <Route path="/team/doctor_shmatov/" component={Shmatov} />
           <Route path="/team/doctor_stolyarov/" component={Stolyarov} />
           <Route path="/team/doctor_kamenskih/" component={Kamenskih} />
+          <Route path="/team/doctor_kim/" component={Kim} />
           <Route path="/team" component={OurTeam} />
           <Route path="/licenses" component={Licenses} />
           <Route exact path="/" component={Landing} />
