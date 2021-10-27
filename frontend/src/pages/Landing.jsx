12088969470603b618e9a74b2dbcf0b4bee5d505
Ekernik <ekernik@gmail.com>
2021-10-27 14:28:35 +0300

    Fix React-Helmet meta tags

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -6,10 +6,18 @@ import Greetings from "../components/Greetings/";
 import Contacts from "../components/Contacts/";
 import NewsSlider from "../components/NewsSlider/";
 import Banner from "../organisms/Banner/";
+import { Helmet } from "react-helmet";
 
 function Landing() {
   return (
     <main className="landing-page">
+      <Helmet>
+        <title>Центр кардиохирургии и интервенционной кардиологии</title>
+        <meta
+          name="description"
+          content="Кардиология, кардиохирургия, сосудистая хирургия, аритмология - в клинике ВМТ им. Н.И.Пирогова. Хирургическое лечение заболеваний органов сердечно-сосудистой системы."
+        />
+      </Helmet>
       <Greetings
         header="Центр кардиохирургии и интервенционной кардиологии"
         subheader="Медицина, доступная каждому"
