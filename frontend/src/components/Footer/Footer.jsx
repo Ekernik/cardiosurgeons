3fe4da674631180997d271e3e67165c44d6932a0
Ekernik <ekernik@gmail.com>
2021-10-29 22:42:56 +0300

    Add instagram link in Footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,13 +1,30 @@
-import React from "react";
-import "./index.scss";
-import reestr from "../../assets/documents/reestr.pdf";
-import FAB from "../FAB/FAB";
+import React from 'react';
+import './index.scss';
+import reestr from '../../assets/documents/reestr.pdf';
+import FAB from '../FAB/FAB';
+import insta_logo from '../../assets/svg/instagram.svg';
 
 function Footer() {
   return (
     <footer className="footer">
       <FAB />
-      <div className="container">
+      <div className="container text-center">
+        <div className="social-links">
+          <a
+            href="https://www.instagram.com/heartteam.spb/"
+            target="_blank"
+            className="social-links__link"
+            rel="noreferrer"
+          >
+            <img
+              src={insta_logo}
+              alt="ссылка на наш instagram"
+              className="social-links__icon"
+            />
+            heartteam.spb
+          </a>
+        </div>
+        <hr className="footer__hr" />
         <span className="footer__copyright">© copyright 2021</span>
         <a
           href={reestr}
