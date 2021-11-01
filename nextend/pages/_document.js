79f8c447436a6a594a05b1252a2a8a05c593fcb4
Ekernik <ekernik@gmail.com>
2021-11-01 13:32:50 +0300

    Add static files

--- nextend/pages/_document.js
+++ nextend/pages/_document.js
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   render() {
@@ -17,6 +17,45 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          {/* <!-- Yandex.Metrika counter --> */}
+          <script type="text/javascript">
+            {(function (m, e, t, r, i, k, a) {
+              m[i] =
+                m[i] ||
+                function () {
+                  (m[i].a = m[i].a || []).push(arguments);
+                };
+              m[i].l = 1 * new Date();
+              (k = e.createElement(t)),
+                (a = e.getElementsByTagName(t)[0]),
+                (k.async = 1),
+                (k.src = r),
+                a.parentNode.insertBefore(k, a);
+            })(
+              window,
+              document,
+              "script",
+              "https://mc.yandex.ru/metrika/tag.js",
+              "ym"
+            )}
+
+            {ym(83857552, "init", {
+              clickmap: true,
+              trackLinks: true,
+              accurateTrackBounce: true,
+              webvisor: true,
+            })}
+          </script>
+          <noscript>
+            <div>
+              <img
+                src="https://mc.yandex.ru/watch/83857552"
+                style="position: absolute; left: -9999px"
+                alt=""
+              />
+            </div>
+          </noscript>
+          {/* <!-- /Yandex.Metrika counter --> */}
         </body>
       </Html>
     );
