025319a90fff690838bb9f3c67d9d82de919b578
Ekern Nikita <ekernik@gmail.com>
2021-11-19 14:46:03 +0300

    remove old version of the site and add new titles and descriptions

--- nextend/pages/treatment/transkatetornaya-implantaciya-aortalnogo-klapana.jsx
+++ nextend/pages/treatment/transkatetornaya-implantaciya-aortalnogo-klapana.jsx
@@ -11,6 +11,21 @@ export default function Tavi() {
     <>
       <Head>
         <title>Транскатетерная имплантация аортального клапана</title>
+        <meta
+          name="description"
+          content="Проводим малоинвазивные операции по транскатетерной имплантации аортального клапана. Записывайтесь на прием к специалистам нашей клиники."
+        />
+        <meta
+          property="og:title"
+          content="Транскатетерная имплантация аортального клапана"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="./image.webp" />
+        <meta
+          property="og:description"
+          content="Проводим малоинвазивные операции по транскатетерной имплантации аортального клапана. Записывайтесь на прием к специалистам нашей клиники."
+        />
+        <meta property="og:url" content="https://heartteamspb.com/treatment/transkatetornaya-implantaciya-aortalnogo-klapana" />
       </Head>
       <div className="App">
         <div id="curtain"></div>
