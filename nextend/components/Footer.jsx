89c4d5e47262b0e4b7ce42146932b4b6ea862e0b
Ekernik <ekernik@gmail.com>
2021-12-12 14:02:35 +0300

    Add VK to social links in Footer

--- nextend/components/Footer.jsx
+++ nextend/components/Footer.jsx
@@ -1,23 +1,36 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import insta_logo from '../public/static/svg/instagram.svg';
+import logo_instagram from '../public/static/svg/instagram.svg';
+import logo_vk from '../public/static/svg/vk.svg';
 import styles from '../styles/footer.module.scss';
 // import FAB from '../FAB/FAB'; #TODO
 
 export default function Footer() {
+  const link_instagram = 'https://www.instagram.com/heartteam.spb/';
+  const link_vk = 'https://vk.com/heartteam_spb';
+
   return (
     <footer className={styles.footer}>
       {/* <FAB />  #TODO */}
       <div className="container text-center">
         <div className={styles.social_links}>
           <a
-            href="https://www.instagram.com/heartteam.spb/"
+            href={link_instagram}
             target="_blank"
             className={styles.social_links__link}
             rel="noreferrer noopenner"
           >
-            <Image src={insta_logo} alt="ссылка на наш instagram" />
+            <Image src={logo_instagram} alt="Ссылка на наш instagram." />
             <span className={styles.social_links__icon}>heartteam.spb</span>
+          </a>
+          <a
+            href={link_vk}
+            target="_blank"
+            className={styles.social_links__link}
+            rel="noreferrer noopenner"
+          >
+            <Image src={logo_vk} alt="Cсылка на нас во Вконтакте." />
+            <span className={styles.social_links__icon}>heartteam_spb</span>
           </a>
         </div>
         <hr className={styles.footer__hr} />
