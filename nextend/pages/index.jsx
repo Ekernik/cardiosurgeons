d730aa38f73fd1dc805501d3ca4c831f1d873bd7
Nikita Ekern <ekernik@gmail.com>
2022-01-17 20:03:04 +0300

    refactor(index.js): change Head to SEO component

--- nextend/pages/index.jsx
+++ nextend/pages/index.jsx
@@ -7,67 +7,47 @@ import Banner from '@/components/Banner';
 import NewsSlider from '@/components/NewsSlider';
 import Contacts from '@/components/Contacts';
 import ReviewSlider from '@/components/ReviewSlider';
+import SEO from '@/components/SEO';
 
 function App() {
+  const SEODetails = {
+    title: 'Центр кардиохирургии и интервенционной кардиологии',
+    description:
+      'Кардиология, кардиохирургия, сосудистая хирургия, аритмология - в клинике ВМТ им. Н.И.Пирогова. Хирургическое лечение заболеваний органов сердечно-сосудистой системы.',
+    pageLink: 'https://heartteamspb.com',
+  };
+
   return (
     <>
-      <Head>
-        <title>Центр кардиохирургии и интервенционной кардиологии</title>
-        <meta
-          name="description"
-          content="Кардиология, кардиохирургия, сосудистая хирургия, аритмология - в клинике ВМТ им. Н.И.Пирогова. Хирургическое лечение заболеваний органов сердечно-сосудистой системы."
-          data-react-helmet="true"
-        />
-        <meta
-          property="og:title"
-          content="Центр кардиологии и интервенционной кардиологии."
-          data-react-helmet="true"
-        />
-        <meta property="og:type" content="website" data-react-helmet="true" />
-        <meta
-          property="og:image"
-          content="./image.webp"
-          data-react-helmet="true"
-        />
-        <meta
-          property="og:description"
-          content="Кардиология, кардиохирургия, сосудистая хирургия, аритмология - в клинике ВМТ им. Н.И.Пирогова. Хирургическое лечение заболеваний органов сердечно-сосудистой системы."
-          data-react-helmet="true"
-        />
-        <meta
-          property="og:url"
-          content="heartteamspb.com"
-          data-react-helmet="true"
-        />
-      </Head>
-      <div className="App">
-        <div id="curtain"></div>
+      <SEO details={SEODetails} />
+      <div className='App'>
+        <div id='curtain'></div>
         <Header />
-        <main className="landing-page">
+        <main className='landing-page'>
           <Greetings
-            header="Центр кардиохирургии и интервенционной кардиологии"
-            subheader="Медицина, доступная каждому"
-            buttonText="Записаться на прием"
+            header='Центр кардиохирургии и интервенционной кардиологии'
+            subheader='Медицина, доступная каждому'
+            buttonText='Записаться на прием'
           />
           <Banner />
-          <section className="container">
-            <h2 className="article__header">
+          <section className='container'>
+            <h2 className='article__header'>
               Центр кардиохирургии и интервенционной кардиологии
             </h2>
-            <p className="article__p">
+            <p className='article__p'>
               Центр кардиохирургии и интервенционной кардиологии приглашает
               на&nbsp;прием,{' '}
-              <Link href="/diagnostics">
-                <a className="article__link">диагностику</a>
+              <Link href='/diagnostics'>
+                <a className='article__link'>диагностику</a>
               </Link>{' '}
               и&nbsp;
-              <Link href="/treatment">
-                <a className="article__link">лечение</a>
+              <Link href='/treatment'>
+                <a className='article__link'>лечение</a>
               </Link>{' '}
               пациентов с&nbsp;заболеваниями сердца и&nbsp;сердечно-сосудистой
               системы.
             </p>
-            <p className="article__p">
+            <p className='article__p'>
               В нашей клинике работают специалисты разных направлений
               деятельности: кардиологии, кардиохирургии, аритмологии, сосудистой
               хирургии, кардиоанестезиологии и&nbsp;др. Благодаря их слаженной
@@ -79,15 +59,15 @@ function App() {
               у&nbsp;пациента острой патологии, он&nbsp;будет незамедлительно
               направлен на&nbsp;консультацию к&nbsp;профильному специалисту.
             </p>
-            <p className="article__p">
+            <p className='article__p'>
               Для комфортного пребывания пациентов наше отделение располагает
               одноместными, двух- и&nbsp;трехместными палатами со&nbsp;всеми
               удобствами.
             </p>
-            <p className="article__p">
+            <p className='article__p'>
               Для записи на прием к специалистам Центра кардиохирургии и
               интервенционной кардиологии, звоните по телефону{' '}
-              <a href="tel:+79992380136" className="article__link">
+              <a href='tel:+79992380136' className='article__link'>
                 +7&nbsp;(999)&nbsp;238-01-36
               </a>
               .
