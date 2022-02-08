def17e3e2720fda22b00fc1117cc2084ce71b904
Ekernik <ekernik@gmail.com>
2022-02-08 13:31:40 +0300

    feat(review): add new review

---
+++ nextend/components/Reviews/16.jsx
@@ -0,0 +1,16 @@
+import Link from '@/components/Link';
+
+export default function Review({ onClose }) {
+  return (
+    <article>
+      <p className='article__p'>
+        Хочу от всей души поблагодарить весь мед персонал.
+      </p>
+      <p className='article__p'>
+        Я не знал, что так могут работать врачи. Это небо и земля. Все
+        аккуратно, без суеты. Все расскажут, что спросишь.
+      </p>
+      <p className='article__p'>Очень доволен. Дай бог им здоровья</p>
+    </article>
+  );
+}
