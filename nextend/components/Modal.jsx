489b1fe6db9e350225859cb21beeb73df88bb819
Ekernik <ekernik@gmail.com>
2022-02-17 12:28:55 +0300

    refactor(modal): remove unnecessary divs and change to singlequotes

--- nextend/components/Modal.jsx
+++ nextend/components/Modal.jsx
@@ -1,26 +1,24 @@
-import Image from "next/image";
-import React, { Suspense } from "react";
-import close_img from "@/public/static/svg/x-mark.png";
-import styles from "@/styles/modal.module.scss";
+import Image from 'next/image';
+import React, { Suspense } from 'react';
+import close_img from '@/public/static/svg/x-mark.png';
+import styles from '@/styles/modal.module.scss';
 
 export default function Modal({ open, onClose, content }) {
   if (!open) return null;
   const ContentfullComponent = React.lazy(() => import(`./Reviews/${content}`));
   return (
-    <>
-      <div className={styles.overlay} onClick={onClose}>
-        <div className={styles.modal} onClick={(e) => e.stopPropagation()}>
-          <h3 className={styles.heading}>Отзыв</h3>
-          <div className={styles.container}>
-            <button onClick={onClose} className={styles.button}>
-              <Image src={close_img} alt="" className={styles.close__btn} />
-            </button>
-          </div>
-          <Suspense fallback={<div>Отзыв загружается...</div>}>
-            <ContentfullComponent onClose={onClose} />
-          </Suspense>
+    <div className={styles.overlay} onClick={onClose}>
+      <div className={styles.modal} onClick={e => e.stopPropagation()}>
+        <h3 className={styles.heading}>Отзыв</h3>
+        <div className={styles.container}>
+          <button onClick={onClose} className={styles.button}>
+            <Image src={close_img} alt='' className={styles.close__btn} />
+          </button>
         </div>
+        <Suspense fallback={<div>Отзыв загружается...</div>}>
+          <ContentfullComponent onClose={onClose} />
+        </Suspense>
       </div>
-    </>
+    </div>
   );
 }
