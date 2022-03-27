06f30ec1b34bd37dfabdf1e27a07c9b639c8d38e
Ekernik <ekernik@gmail.com>
2022-03-27 21:08:35 +0300

    refactor(treatment): change to TS, move to folder, change header text

--- nextend/pages/treatment/index.tsx
+++ nextend/pages/treatment/index.tsx
@@ -1,35 +1,33 @@
 import Link from 'next/link';
-import Head from 'next/head';
+import MyLink from '@/components/Link';
+import PhoneLink from '@/components/PhoneLink';
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer';
 import Contacts from '@/components/Contacts';
 import { treatments } from '@/public/static/databases/links';
+import SEO from '@/components/SEO';
+import { NextPage } from 'next';
 
-export default function Treatment() {
-  const title =
-    'Кардиохирургические операции на сердце. Лечение болезней сердца и сосудов.';
-  const description =
-    'Записывайтесь на лечение болезней сердца к ведущим кардиологам и кардиохирургам нашей клиники. Прием по федеральным квотам и ОМС.';
+const Treatment: NextPage = () => {
+  const SEODetails = {
+    title:
+      'Кардиохирургические операции на сердце. Лечение болезней сердца и сосудов.',
+    description:
+      'Записывайтесь на лечение болезней сердца к ведущим кардиологам и кардиохирургам нашей клиники. Прием по федеральным квотам и ОМС.',
+    pageLink: 'https://heartteamspb.com/treatment',
+    type: 'article',
+  };
   return (
     <>
-      <Head>
-        <title>{title}</title>
-        <meta property='og:title' content={title} />
-        <meta name='description' content={description} />
-        <meta property='og:type' content='website' />
-        <meta
-          property='og:image'
-          content='https://heartteamspb.com/static/images/bg-doctors.webp'
-        />
-        <meta property='og:description' content={description} />
-        <meta property='og:url' content='https://heartteamspb.com/treatment' />
-      </Head>
+      <SEO details={SEODetails} />
       <div className='App'>
-        <div id='curtain'></div>
+        <div id='curtain' />
         <Header />
         <main className='page__services'>
           <div className='container container__services'>
-            <h1 className='article__header'>Операции</h1>
+            <h1 className='article__header'>
+              Кардиохирургические операции на сердце
+            </h1>
             <div className='services__list'>
               {treatments.links.map((treatment, i) => (
                 <div className='services__link' key={Date.now() + i}>
@@ -49,11 +47,10 @@ export default function Treatment() {
               <p className='article__p'>
                 Мы проводим полный спектр хирургических, гибридных и
                 эндоваскулярных операций при{' '}
-                <Link href='https://heartteamspb.com/diagnostics/ateroskleros'>
-                  <a className='article__link'>
-                    атеросклерозе коронарных артерий
-                  </a>
-                </Link>
+                <MyLink
+                  href='/diagnostics/ateroskleros'
+                  text='атеросклерозе коронарных артерий'
+                />
                 , артерий нижних конечностей, брахиоцефальных артерий, аорты и
                 ее ветвей, аневрзимах аорты и артерий различной локализации.
               </p>
@@ -68,10 +65,8 @@ export default function Treatment() {
                 Более подробную информацию о кардиохирургических операциях и
                 лечении вы можете получить, обратившись к специалистам нашей
                 клиники по номеру телефона{' '}
-                <a href='tel:+79992380136' className='article__link'>
-                  +7 (999) 238-01-36
-                </a>
-                , или написав нам на электронную почту:{' '}
+                <PhoneLink phoneNumber='+7 (999) 238-01-36' />, или написав нам
+                на электронную почту:{' '}
                 <a
                   href='mailto:heartteam.spb@gmail.com'
                   className='article__link'
@@ -88,4 +83,6 @@ export default function Treatment() {
       </div>
     </>
   );
-}
+};
+
+export default Treatment;
